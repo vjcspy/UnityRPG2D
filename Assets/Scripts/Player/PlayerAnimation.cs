066e4b7f71c2a94b2845148b030e6d3e99ca853f
win <mr.vjcspy@gmail.com>
2025-03-23 23:26:32 +0700

    feat: animation when moving

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerAnimation : MonoBehaviour
+    {
+        private static readonly int IsMoving = Animator.StringToHash("isMoving");
+
+        [SerializeField] private Animator animator;
+        [SerializeField] private Rigidbody2D playerRb;
+
+        private void Start()
+        {
+            playerRb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            var isMoving = Mathf.Abs(playerRb.linearVelocity.x) != 0.0f;
+            animator.SetBool(IsMoving, isMoving);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,65 +1,65 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float jumpForce = 2f;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundCheckRadius = 0.2f;
-
-    private Rigidbody2D rb;
-    private float movementInput;
-    private bool isJumpPressed;
-
-    void Awake()
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerMovement : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();
+        [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private float jumpForce = 2f;
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private Transform groundCheck;
+        [SerializeField] private float groundCheckRadius = 0.2f;
+        private bool isJumpPressed;
+        private float movementInput;
 
-        // Nếu chưa gán groundCheck trong Inspector thì tạo tự động
-        if (groundCheck == null)
+        private Rigidbody2D rb;
+
+        private void Awake()
         {
-            GameObject gc = new GameObject("GroundCheck");
-            gc.transform.SetParent(transform);
-            gc.transform.localPosition = new Vector3(0f, -1.36f, 0f); // chỉnh sao cho đúng vị trí chân
-            groundCheck = gc.transform;
+            rb = GetComponent<Rigidbody2D>();
+
+            // Nếu chưa gán groundCheck trong Inspector thì tạo tự động
+            if (groundCheck == null)
+            {
+                var gc = new GameObject("GroundCheck");
+                gc.transform.SetParent(transform);
+                gc.transform.localPosition = new Vector3(0f, -1.36f, 0f); // chỉnh sao cho đúng vị trí chân
+                groundCheck = gc.transform;
+            }
         }
-    }
 
-    void Update()
-    {
-        movementInput = Input.GetAxisRaw("Horizontal");
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        private void Update()
         {
-            isJumpPressed = true;
+            movementInput = Input.GetAxisRaw("Horizontal");
+            if (Input.GetButtonDown("Jump") && IsGrounded()) isJumpPressed = true;
         }
-    }
 
-    void FixedUpdate()
-    {
-        // Di chuyển ngang
-        rb.linearVelocity = new Vector2(movementInput * moveSpeed, rb.linearVelocity.y);
-
-        // Nhảy
-        if (isJumpPressed)
+        private void FixedUpdate()
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isJumpPressed = false;
+            // Di chuyển ngang
+            rb.linearVelocity = new Vector2(movementInput * moveSpeed, rb.linearVelocity.y);
+
+            // Nhảy
+            if (isJumpPressed)
+            {
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                isJumpPressed = false;
+            }
         }
-    }
 
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        if (groundCheck != null)
+        private void OnDrawGizmosSelected()
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+            if (groundCheck != null)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+            }
+        }
+
+        private bool IsGrounded()
+        {
+            return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         }
     }
 }
